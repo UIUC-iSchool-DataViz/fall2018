299ac3e5eab005f6ba00c98f6d96cbd509418c0b
Matthew Turk <matthewturk@gmail.com>
2018-02-11 13:05:34 -0600

    Adding dodo for doit

---
+++ dodo.py
@@ -0,0 +1,28 @@
+# Our build file (using pydoit.org) for generating diagrams from all of our
+# .asy files.
+
+# We assume that all of our asy files are under week*/diagrams/ , and we want
+# to generate them into .svg files.  Sometimes, they generate multiple files,
+# so we'll parse them for the appropriate output filenames.
+
+import os
+import glob
+
+def get_shipouts(fn):
+    outfns = []
+    with open(fn, "r") as f:
+        for line in (_.strip() for _ in f):
+            if line.startswith("shipout"):
+                outfns.append(line[9:-3])
+    return outfns
+
+def task_build_asy():
+    asy_files = sorted(glob.glob("week*/diagrams/*.asy"))
+    for fn in asy_files:
+        cd = os.path.dirname(fn)
+        yield {'basename': 'make_asy',
+                'name': fn,
+                'targets': [os.path.join(cd, _) for _ in get_shipouts(fn)],
+                'file_dep': [fn],
+                'actions': ['asy -cd %s %s' % (cd, os.path.basename(fn))] }
+
